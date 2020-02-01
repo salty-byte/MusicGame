96cf136dd5b3747ab2d635f4b90eeabd5232898e
saltp40 <saltp40@gmail.com>
2020-02-01 23:05:40 +0900

    :sparkles: Add version text to the start scene

---
+++ Assets/Script/Start/VersionText.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// スタート画面にアプリのリリースバージョンを表示するためのText用クラス。
+/// </summary>
+[RequireComponent (typeof (Text))]
+public class VersionText : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start ()
+	{
+		GetComponent<Text> ().text = $"ver. {Application.version}";
+	}
+}
