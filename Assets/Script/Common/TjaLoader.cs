4099d1c815e8ed84e86100a27bc5e05b295f5c5e
saltp40 <saltp40@gmail.com>
2020-09-20 15:25:16 +0900

    :+1: Add a convert function: from tja format to msd format

---
+++ Assets/Script/Common/TjaLoader.cs
@@ -0,0 +1,134 @@
+﻿using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Tja形式の譜面ファイルをこのゲームの譜面形式に変換するためのクラス。
+/// </summary>
+public class TjaLoader
+{
+	public TjaLoader ()
+	{
+	}
+
+	public List<NoteData> Load (string filePath)
+	{
+		// FileReadTest.txtファイルを読み込む
+		string [] data = File.ReadAllLines (filePath);
+
+		int startIdx = 0;
+		for (int i = 0; i < data.Length; i++) {
+			if (data [i] == "#START") {
+				startIdx = i;
+				break;
+			}
+		}
+
+		if (startIdx == 0) {
+			throw new System.Exception ("#STARTが存在しません。");
+		}
+
+		// ヘッダデータ取得
+		for (int i = 0; i < startIdx; i++) {
+
+		}
+
+		// ボディデータ取得
+		bool comma = false;
+		int current = startIdx + 1;
+		float mesure = 1;
+		float offset = 1.9f;
+		float bpm = 128;
+		int dir = 0;
+		int counta = 0;
+		float currentTime = offset;
+		List<NoteData> noteDataList = new List<NoteData> ();
+		for (int i = startIdx + 1; i < data.Length; i++) {
+			if (data [i].Contains (",")) {
+				int allcount = 0;
+				for (int j = current; j <= i; j++) {
+					string line = data [j].Replace (",", "").Trim ();
+					if (line.Length != 0 && line [0] != '#') {
+						allcount += line.Length;
+					}
+					Debug.Log ($"line: {line}");
+				}
+				Debug.Log ($"{current}, {i}: {allcount}");
+
+				for (int j = current; j <= i; j++) {
+					string line = data [j].Replace (",", "").Trim ();
+					if (line.Length != 0 && line [0] == '#') {
+						string [] values = line.Split (' ');
+						switch (values [0]) {
+						case "#BPMCHANGE":
+							bpm = float.Parse (values [1]);
+							Debug.Log ($"bpm: {bpm}");
+							break;
+						case "#MEASURE":
+							string [] h = values [1].Split ('/');
+							float h1 = float.Parse (h [0]);
+							float h2 = float.Parse (h [1]);
+							mesure = h1 / h2;
+							Debug.Log ($"mesure: {mesure}");
+							break;
+						}
+					} else if (line.Length != 0) {
+						float haku = 4 * mesure;
+						float noteTime = 60f / bpm / allcount * haku;
+						Debug.Log ($"haku: {haku}, t: {noteTime}");
+
+						char [] chars = line.ToCharArray ();
+						for (int k = 0; k < chars.Length; k++) {
+							float time = currentTime + noteTime * k;
+							switch (chars [k]) {
+							case '1':  // 小1
+								if (counta % 2 == 0) {
+									counta += 2;
+									dir = (dir + 3) % 6;
+								} else {
+									counta = 0;
+									dir = (dir + 5) % 6;
+								}
+								noteDataList.Add (new NoteData (time, dir));
+								break;
+							case '2':  // 小2
+								if (counta % 2 == 0) {
+									counta = 1;
+									dir = (dir + 5) % 6;
+								} else {
+									counta += 2;
+									dir = (dir + 3) % 6;
+								}
+								noteDataList.Add (new NoteData (time, dir));
+								break;
+							case '3':  // 大1
+								dir = (dir + 3) % 6;
+								noteDataList.Add (new NoteData (time, dir));
+								break;
+							case '4':  // 大2
+								dir = (dir + 3) % 6;
+								noteDataList.Add (new NoteData (time, dir));
+								break;
+							case '6':  // ロングノーツ始まり
+								noteDataList.Add (new NoteData (time, 1));
+								break;
+							case '8':  // ロングノーツ終わり
+								noteDataList.Add (new NoteData (time, 0));
+								break;
+							}
+						}
+						currentTime += (noteTime * chars.Length);
+					}
+				}
+				current = i + 1;
+				comma = true;
+			} else if (comma) {
+				current = i;
+				comma = false;
+			}
+
+		}
+
+		return noteDataList;
+	}
+}
---
+++ Assets/Tests/Editor/Common/TjaLoaderTests.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using UnityEngine;
+using System.IO;
+using Assert = UnityEngine.Assertions.Assert;
+
+namespace Tests
+{
+	/// <summary>
+	/// <c>TjaLoader</c>クラスのテスト。
+	/// </summary>
+	class TjaTests
+	{
+		/// <summary>
+		/// テストデータ読み込み先のパス。
+		/// </summary>
+		public static readonly string DataPath = $"{Application.dataPath}/Tests/Resources/TjaToMsd";
+
+		/// <summary>
+		/// テストデータ出力先のパス。
+		/// </summary>
+		public static readonly string OutputPath = $"{DataPath}/result";
+
+		/// <summary>
+		/// このクラスのテスト開始時に、テスト用のフォルダを作成する。
+		/// </summary>
+		[OneTimeSetUp]
+		public void CreateTestFolder()
+		{
+			var di = new DirectoryInfo(OutputPath);
+			if (!di.Exists)
+			{
+				di.Create();
+			}
+		}
+
+		/// <summary>
+		/// 指定したtjaファイルを読み込み、msdファイルとして出力するテスト。
+		/// </summary>
+		[Test]
+		public void WriteText ()
+		{
+			string name = "Arabesque";
+			string file = $"{DataPath}/{name}.tja";
+			var loader = new TjaLoader ();
+			var result = loader.Load (file);
+
+			var md = new MusicData (name, "6");
+			md.notes = result;
+			md.audioFileName = $"{name}.ogg";
+
+			string json = JsonUtility.ToJson (md);
+			string outputFile = $"{OutputPath}/{name}.msd";
+			FileUtils.WriteText (outputFile, json);
+
+			Assert.IsTrue(new FileInfo(outputFile).Exists, "ファイルが作成されていること。");
+		}
+
+		/// <summary>
+		/// このクラスのテスト終了時に、テスト用のフォルダを削除する。
+		/// </summary>
+		[OneTimeTearDown]
+		public void DeleteFile()
+		{
+			var di = new DirectoryInfo(OutputPath);
+			if (di.Exists)
+			{
+				di.Delete(true);
+			}
+		}
+	}
+}
